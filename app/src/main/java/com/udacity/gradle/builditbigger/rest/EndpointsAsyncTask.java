f2997d6e5b0e6d7e0bad4cd4519843ae1c5c756c
Sudhir Khanger <sudhir@sudhirkhanger.com>
2016-10-11 22:17:08 +0530

    ip change

--- app/src/main/java/com/udacity/gradle/builditbigger/rest/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/rest/EndpointsAsyncTask.java
@@ -40,7 +40,7 @@ public class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
 //                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                    .setRootUrl("http://192.168.1.35:8080/_ah/api/")
+                    .setRootUrl("http://192.168.1.33:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
